6f43788d9e42f47e8d7111af4ef61d9d31bf2a15
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2020-09-16 21:12:58 +0300

    Знакомство_module1_task2_try_1
    Первое задание по учебному проекту.
    попытка_1

---
+++ js/controls.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const fireballSize = 22;
+const getFireballSpeed = function (isMoveToright){
+  return isMoveToLeft ? 5 : 2;
+}
+
+const wizardSpeed = 3;
+const wizardWidth = 70;
+const getWizardHeight = function(){
+  return 1.337 * wizardWidth;
+};
+
+const getWizardX = function(gameFlorWidth){
+return gameFlorWidth / 2;
+};
+
+const getWizardX = function(gameFlorHeight){
+return gameFlorHeight / 3;
+};
