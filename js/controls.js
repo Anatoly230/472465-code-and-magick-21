8abc006f1879c3b67c5b2856e282bef62a7de069
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2020-09-28 23:12:30 +0300

    Гистограмм - 1я попытка
    Гистограмм - 1я попытка

--- js/controls.js
+++ js/controls.js
@@ -1,20 +1,20 @@
-"use strict"
+"use strict";
 
 var fireballSize = 22;
-var getFireballSpeed = function (isMoveToLeft){
+var getFireballSpeed = function (isMoveToLeft) {
   return isMoveToLeft ? 2 : 5;
-}
+};
 
 var wizardSpeed = 3;
 var wizardWidth = 70;
-var getWizardHeight = function(){
+var getWizardHeight = function () {
   return 1.337 * wizardWidth;
 };
 
-var getWizardX = function(gameFlorWidth){
+var getWizardX = function (gameFlorWidth) {
   return (gameFlorWidth - wizardWidth) / 2;
 };
 
-var getWizardY = function(gameFlorHeight){
+var getWizardY = function (gameFlorHeight) {
   return gameFlorHeight / 3;
 };
