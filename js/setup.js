129c6139206905a1444bb278244b1860a4342f9b
Anatoly230 <65662008+Anatoly230@users.noreply.github.com>
2020-10-05 16:00:59 +0300

    smilar window initialisation try-3
    Инициализация окна для поиска похожих Пендальфов.
    Попытка-3

--- js/setup.js
+++ js/setup.js
@@ -1,96 +1,95 @@
+'use strict';
 const names = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'];
+  `Иван`,
+  `Хуан Себастьян`,
+  `Мария`,
+  `Кристоф`,
+  `Виктор`,
+  `Юлия`,
+  `Люпита`,
+  `Вашингтон`];
 
 const surNames = [
-'да Марья',
-'Верон',
-'Мирабелла',
-'Вальц',
-'Онопко',
-'Топольницкая',
-'Нионго',
-'Ирвинг'];
+  `да Марья`,
+  `Верон`,
+  `Мирабелла`,
+  `Вальц`,
+  `Онопко`,
+  `Топольницкая`,
+  `Нионго`,
+  `Ирвинг`];
 
 const coatColors = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'];
+  `rgb(101, 137, 164)`,
+  `rgb(241, 43, 107)`,
+  `rgb(146, 100, 161)`,
+  `rgb(56, 159, 117)`,
+  `rgb(215, 210, 55)`,
+  `rgb(0, 0, 0)`];
 
 const eyesColors = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'];
+  `black`,
+  `red`,
+  `blue`,
+  `yellow`,
+  `green`];
 
-let setupWindow = document.querySelector('.setup').classList.remove('hidden');
-let similarIconTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-let similarsList = document.querySelector('.setup-similar-list');
+document.querySelector(`.setup`).classList.remove(`hidden`);
+let similarIconTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+let similarsList = document.querySelector(`.setup-similar-list`);
 let wizards = [];
 
-let selectFromArray = (array) => {
-
-let arrElement = array.shift();
-let selectElement = arrElement;
-array.push(arrElement);
-
-return selectElement
+let getRandomInt = (max) => {
+  return (Math.floor(Math.random() * Math.floor(max)));
 };
 
-let initialObj = (names, surNames, coatColors, eyesColors) => {
+let selectFromArray = (array) => {
+  let max = array.length;
+  let element = getRandomInt(max);
+  return array[element];
+};
 
-let obj = {};
 
-obj.name = selectFromArray(names);
-obj.surName = selectFromArray(surNames);
-obj.coatColor = selectFromArray(coatColors);
-obj.eyesColor = selectFromArray(eyesColors);
+let initialObj = (name, surName, coatColor, eyesColor) => {
 
-return obj
-  };
+  let obj = {};
+
+  obj.name = selectFromArray(name);
+  obj.surName = selectFromArray(surName);
+  obj.coatColor = selectFromArray(coatColor);
+  obj.eyesColor = selectFromArray(eyesColor);
+
+  return obj;
+};
 
 for (let i = 0; i < 4; i++) {
-wizards[i] = initialObj(names, surNames, coatColors, eyesColors);
-};
-
-
-
-
+  wizards[i] = initialObj(names, surNames, coatColors, eyesColors);
+}
 
 let createIcon = (arrayObj) => {
 
-let copySimilarItem = similarIconTemplate.cloneNode(true);
+  let copySimilarItem = similarIconTemplate.cloneNode(true);
 
-copySimilarItem.querySelector('.setup-similar-label').textContent = arrayObj.name + ' ' + arrayObj.surName;
-copySimilarItem.querySelector('.wizard-coat').style.fill = arrayObj.coatColor;
-copySimilarItem.querySelector('.wizard-eyes').style.fill = arrayObj.eyesColor;
+  copySimilarItem.querySelector(`.setup-similar-label`).textContent = arrayObj.name + ` ` + arrayObj.surName;
+  copySimilarItem.querySelector(`.wizard-coat`).style.fill = arrayObj.coatColor;
+  copySimilarItem.querySelector(`.wizard-eyes`).style.fill = arrayObj.eyesColor;
 
-return copySimilarItem
+  return copySimilarItem;
 
 };
 
 
 let renderElements = () => {
 
-let fragment = document.createDocumentFragment();
+  let fragment = document.createDocumentFragment();
 
-for (object of wizards) {
-fragment.appendChild(createIcon(object));
-};
+  for (let object of wizards) {
+    fragment.appendChild(createIcon(object));
+  }
 
-similarsList.appendChild(fragment);
+  similarsList.appendChild(fragment);
 
-let similarsBlock = document.querySelector('.setup-similar').classList.remove('hidden');
+  document.querySelector(`.setup-similar`).classList.remove(`hidden`);
 
 };
 
